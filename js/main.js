6f34a3c1ad758979d58cc4a8bead6a7ed20ff252
johncater <john.cater@cjrw.com>
2015-10-28 08:35:41 -0500

    added additional browser styles

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).on("keypress", "form", function(event) {
     return event.keyCode != 13;
 });
 
+
 $('.input').keyup(function(){
 	$('.clear').click(function() {
         $('label.instructions').removeClass('selected');
@@ -15,7 +16,7 @@ $('.input').keyup(function(){
     if( outputnumber.length < 10 )
     {
         $('.bryceoutput').css("font-size","12em");
-    } else if ( outputnumber.length > 30 )
+    } else if ( outputnumber.length > 25 )
     {
         $('.bryceoutput').css("font-size","8em");
     } else 
@@ -24,15 +25,38 @@ $('.input').keyup(function(){
     }
 });
 
-// Label mover
-$('input#bryceinput').focus(function(){
-    $('label.instructions').addClass('selected');
+$(document).ready(function() {
+    // Label mover
+    $('input#bryceinput').focus(function(){
+        $('label.instructions').addClass('selected');
+    });
+
+    var text_value = $(this).val();
+    if(text_value == '')
+    {
+        $('input#bryceinput').blur(function(){
+            $('label.instructions').removeClass('selected'); 
+        });  
+    }
 });
 
-var text_value = $(this).val();
-if(text_value == '')
-{
-    $('input#bryceinput').blur(function(){
-        $('label.instructions').removeClass('selected'); 
-    });  
-}
+
+/* 
+ $('.sharebtn').click(function(){
+    $('.imagewrapper').addClass('show');
+    
+     var canvas = document.getElementById("myCanvas");
+     var context = canvas.getContext("2d");
+     var input = $('input#bryceinput').val();
+     var imageObj = new Image();
+     var angle = -0.2;
+     var angle2 = 0.05;
+     imageObj.onload = function(){
+         context.drawImage(imageObj, 10, 10);
+         context.font = "60pt Just Another Hand";
+         context.setTransform(1, angle2, angle, 1, 0, 0)
+         context.fillText(input, 90, 200);
+         context.setTransform (1, 0, 0, 1, 0, 0);
+     };
+     imageObj.src = "img/sharebg.jpg"; 
+}); */
