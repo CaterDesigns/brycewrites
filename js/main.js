dce22be478d52188def41e30be09526c5ed3f86c
johncater <john.cater@cjrw.com>
2015-11-02 08:56:44 -0600

    updated sharing options

--- js/main.js
+++ js/main.js
@@ -2,35 +2,55 @@ $(document).on("keypress", "form", function(event) {
     return event.keyCode != 13;
 });
 
+$('.clear').click(function() {
+    $('label.instructions').removeClass('selected');
+    $('.input').val('');
+    $('.bryceoutput').text($('.input').val());
+});
 
-$('.input').keyup(function(){
-	$('.clear').click(function() {
-        $('label.instructions').removeClass('selected');
-		$('.input').val('');
-		$('.bryceoutput').text($('.input').val());
-	});
+$('.input').on('keyup change', function(){
 	$('.bryceoutput').text($(this).val());
     
-    var outputnumber = $('input#bryceinput').val();
+    $('.addthis_native_toolbox').attr('data-url', 'http://127.0.0.1:8000/index.html?m=' + btoa($('.bryceoutput').text()));
+    
+    var outputnumber = $('.bryceoutput').text();
 
-    if( outputnumber.length < 10 )
-    {
-        $('.bryceoutput').css("font-size","12em");
-    } else if ( outputnumber.length > 25 )
-    {
-        $('.bryceoutput').css("font-size","8em");
-    } else 
-    {
-        $('.bryceoutput').css("font-size","10em");
+    if (outputnumber.length < 10) {
+        $('.bryceoutput').css("font-size", "12em");
+    } else if (outputnumber.length > 25) {
+        $('.bryceoutput').css("font-size", "8em");
+    } else {
+        $('.bryceoutput').css("font-size", "10em");
     }
+    
+    addthis.update('share', 'url', window.location.href + btoa($('.bryceoutput').text()));
 });
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
 
 $(document).ready(function() {
     // Label mover
     $('input#bryceinput').focus(function(){
         $('label.instructions').addClass('selected');
     });
-
+    
+    if ('m' in getUrlVars() ) {
+        var getUrl = window.atob(getUrlVars().m);
+        $('.bryceoutput').text(getUrl);
+        
+        $('form').css("display","none");
+        $('.createnew').css("display","block");
+    } else {
+        $('.createnew').css("display","none");
+    }
+        
     var text_value = $(this).val();
     if(text_value == '')
     {
@@ -38,25 +58,14 @@ $(document).ready(function() {
             $('label.instructions').removeClass('selected'); 
         });  
     }
-});
-
-
-/* 
- $('.sharebtn').click(function(){
-    $('.imagewrapper').addClass('show');
     
-     var canvas = document.getElementById("myCanvas");
-     var context = canvas.getContext("2d");
-     var input = $('input#bryceinput').val();
-     var imageObj = new Image();
-     var angle = -0.2;
-     var angle2 = 0.05;
-     imageObj.onload = function(){
-         context.drawImage(imageObj, 10, 10);
-         context.font = "60pt Just Another Hand";
-         context.setTransform(1, angle2, angle, 1, 0, 0)
-         context.fillText(input, 90, 200);
-         context.setTransform (1, 0, 0, 1, 0, 0);
-     };
-     imageObj.src = "img/sharebg.jpg"; 
-}); */
+    var outputnumber = $('.bryceoutput').text();
+
+    if (outputnumber.length < 10) {
+        $('.bryceoutput').css("font-size", "12em");
+    } else if (outputnumber.length > 25) {
+        $('.bryceoutput').css("font-size", "8em");
+    } else {
+        $('.bryceoutput').css("font-size", "10em");
+    }
+});
