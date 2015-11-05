ea5e39eccc4ac9773a747b9c45b1a24b2fe67a7a
johncater <john.cater@cjrw.com>
2015-11-05 10:00:43 -0600

    added sharing functionality

--- js/main.js
+++ js/main.js
@@ -1,71 +1,113 @@
-$(document).on("keypress", "form", function(event) { 
-    return event.keyCode != 13;
-});
-
-$('.clear').click(function() {
-    $('label.instructions').removeClass('selected');
-    $('.input').val('');
-    $('.bryceoutput').text($('.input').val());
-});
-
-$('.input').on('keyup change', function(){
-	$('.bryceoutput').text($(this).val());
+$(document).ready(function() {
     
-    $('.addthis_native_toolbox').attr('data-url', 'http://127.0.0.1:8000/index.html?m=' + btoa($('.bryceoutput').text()));
-    
-    var outputnumber = $('.bryceoutput').text();
+    $(document).on("keypress", "form", function(event) { 
+        return event.keyCode != 13;
+    });
 
-    if (outputnumber.length < 10) {
+    // Clear input
+    $('.clear').click(function() {
+        $('form').removeClass('selected');
+        $('.input').val('');
+        $('.bryceoutput').text($('.input').val());
+    });
+
+    // Change message based on input
+    $('.input').on('keyup change', function(){
+        $('.bryceoutput').text($(this).val());
+
+        var outputnumber = $('.bryceoutput').text();
+
+        if (outputnumber.length < 10) {
         $('.bryceoutput').css("font-size", "12em");
-    } else if (outputnumber.length > 25) {
-        $('.bryceoutput').css("font-size", "8em");
-    } else {
-        $('.bryceoutput').css("font-size", "10em");
+        } else if (outputnumber.length > 25 && outputnumber.length < 45) {
+            $('.bryceoutput').css("font-size", "8em");
+        } else if (outputnumber.length > 45) {
+            $('.bryceoutput').css("font-size", "6em");
+        } else {
+            $('.bryceoutput').css("font-size", "10em");
+        }
+
+        // $('.addthis_native_toolbox').attr('data-url', 'http://brycewrites.com/?m=' + btoa($('.bryceoutput').text()));
+        var currentUrl = window.location;
+        $('.sharebtn').attr('href', currentUrl + '?m=' + btoa($('.bryceoutput').text()));
+        
+    });
+
+    // Query function
+    function getUrlVars() {
+        var vars = {};
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+            vars[key] = value;
+        });
+        return vars;
     }
     
-    addthis.update('share', 'url', window.location.href + btoa($('.bryceoutput').text()));
-});
-
-
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
-        vars[key] = value;
-    });
-    return vars;
-}
-
-$(document).ready(function() {
     // Label mover
     $('input#bryceinput').focus(function(){
-        $('label.instructions').addClass('selected');
+        $('form').addClass('selected');
     });
     
+    var text_value = $('input#bryceinput').val();
+    if(text_value != '')
+    {
+        $('input#bryceinput').blur(function(){
+            $('form').removeClass('selected'); 
+        });  
+    }
+    
+    // Checks for query and hides UI
     if ('m' in getUrlVars() ) {
         var getUrl = window.atob(getUrlVars().m);
         $('.bryceoutput').text(getUrl);
         
         $('form').css("display","none");
         $('.createnew').css("display","block");
+        $('.sharebtn').css("display","none");
     } else {
         $('.createnew').css("display","none");
-    }
-        
-    var text_value = $(this).val();
-    if(text_value == '')
-    {
-        $('input#bryceinput').blur(function(){
-            $('label.instructions').removeClass('selected'); 
-        });  
+        $('.addthis_native_toolbox').css("display","none");
     }
     
+    // Adjusts text size of output for initial load.
     var outputnumber = $('.bryceoutput').text();
 
     if (outputnumber.length < 10) {
         $('.bryceoutput').css("font-size", "12em");
-    } else if (outputnumber.length > 25) {
+    } else if (outputnumber.length > 25 && outputnumber.length < 45) {
         $('.bryceoutput').css("font-size", "8em");
+    } else if (outputnumber.length > 45) {
+        $('.bryceoutput').css("font-size", "6em");
     } else {
         $('.bryceoutput').css("font-size", "10em");
     }
 });
+/* 
+var hashTO;
+$('#myTextField').on('keyup', function () {
+    if (hashTO !== undefined) {
+        clearTimeout(hashTO);
+    }
+    hashTO = setTimeout(updateHash, 1000);
+});
+
+function updateHash() {
+    var hashValEncoded = btoa($('#myTextField').val());
+    window.location.hash = hashValEncoded;
+    $('.testoutput').text(hashValEncoded);
+}
+
+$('#myTextField').on('keyup change', function() {
+    var hashVal = $('#myTextField').val();
+    $('.alphaoutput').text(hashVal);
+}); 
+
+$('.sharebtn').on('click', function(evt) {
+    evt.preventDefault();
+    var $this = $(this),
+    target = $this.data('target');
+
+    var querystring = btoa($('.bryceoutput').text());
+    $('#wrapper').load(target + ".html?=" + querystring);
+});
+
+*/
